f5a03f675dcd5d2a77f9612f604c1c9da1530682
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-06-16 18:35:00 +0200

    almost finished

--- src/Views/DetailCountry.jsx
+++ src/Views/DetailCountry.jsx
@@ -6,79 +6,107 @@ import Home from "./Home"
 const DetailCountry = () => {
   const location = useLocation()
   const country = location.state
-  console.log(country)
+
+  let nativeName
+  for (const name in country) {
+    const nat = country.name.nativeName
+    for (const name in nat) {
+      nativeName = nat[name].common
+    }
+  }
+
+  let languages
+  for (const language in country) {
+    languages = Object.values(country.languages)
+  }
+
+  let currencies
+  for (const currency in country) {
+    currencies = Object.values(country.currencies)
+  }
   return (
     <>
       <Bar />
-
-      <Link className="card" to="/" component={<Home />}>
-        <button className="back-btn">
-          <span>
-            <i className="fa-solid fa-left-long"></i>
-          </span>
-          Back
-        </button>
-      </Link>
-
       <div className="detail" key={country.flags.svg}>
-        <div className="flag">
-          <img
-            src={country.flags.svg}
-            alt="flag"
-            srcSet=""
-            width="300px"
-            height="200px"
-          />
+        <div className="back-btn">
+          <Link className="back" to="/" component={<Home />}>
+            <button className="back-btn">
+              <span>
+                <i className="fa-solid fa-left-long"></i>
+              </span>
+              Back
+            </button>
+          </Link>
         </div>
 
-        <div className="detailData">
-          <div className="name">{country.name.common}</div>
-          <div className="details">
-            <div className="first-column">
-              <p>
-                Native Name:
-                {/* {country.name.nativeName.spa.common} */}
-              </p>
-              <p>
-                Population:
-                {country.population.toLocaleString()}
-              </p>
-              <p>
-                Region:
-                {country.region}
-              </p>
-              <p>
-                Sub Region:
-                {country.subregion}
-              </p>
-              <p>
-                Capital:
-                {country.capital}
-              </p>
-            </div>
+        <div className="alignment-detail">
+          <div className="flag-detail">
+            <img
+              src={country.flags.svg}
+              alt="flag"
+              srcSet=""
+              width="350px"
+              height="250px"
+            />
+          </div>
 
-            <div className="second-column">
-              <p>
-                Top Level Domain:
-                {country.tld}
-              </p>
-              <p>
-                Currencies:
-                {/* {country.currencies.USD.symbol} */}
-              </p>
-              <p>
-                Languages:
-                {/* {country.languages.spa} */}
-              </p>
-            </div>
+          <div className="detailData">
+            <div className="name">{country.name.common}</div>
+            <div className="details">
+              <div className="first-column">
+                <p>
+                  <span className="detail-info">Native Name:</span>
+                  {nativeName}
+                </p>
+                <p>
+                  <span className="detail-info">Population:</span>
+                  {country.population.toLocaleString()}
+                </p>
+                <p>
+                  <span className="detail-info">Region:</span>
+                  {country.region}
+                </p>
+                <p>
+                  <span className="detail-info">Sub Region:</span>
+                  {country.subregion}
+                </p>
+                <p>
+                  <span className="detail-info">Capital:</span>
+                  {country.capital}
+                </p>
+              </div>
 
-            <div className="bottom-column">
-              <p>
-                Border Countries:
-                {country.borders.map((border) => (
-                  <button className="borders">{border}</button>
-                ))}
-              </p>
+              <div className="second-column">
+                <p>
+                  <span className="detail-info">Top Level Domain:</span>
+                  {country.tld}
+                </p>
+                <p>
+                  <span className="detail-info">Currencies:</span>
+                  {currencies.map((currency) => `${currency.name}, `)}
+                </p>
+                <p>
+                  <span className="detail-info">Languages:</span>
+                  {languages.map((language) => `${language},  `)}
+                </p>
+              </div>
+
+              <div className="bottom-column">
+                <p>
+                  <span className="detail-info">Border Countries:</span>
+                  {country.borders === undefined ? (
+                    <></>
+                  ) : (
+                    <>
+                      {country.borders.map((border) => (
+                        <button key={border} className="borders">
+                          {border}
+                        </button>
+                      ))}
+                    </>
+                  )}
+                </p>
+              </div>
             </div>
           </div>
         </div>
